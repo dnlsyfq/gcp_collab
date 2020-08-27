59aae74272fd252df4a758bbef6a32ac9f68d2d0
Danial Jalaluddin <dan90@live.com.my>
2020-08-27 12:52:36 +0800

    updated

---
+++ while.py
@@ -0,0 +1,31 @@
+# x = 0
+# while x < 5:
+# 	print("Not there yet, x=" + str(x))
+# 	x = x + 1
+# print("x=" + str(x))
+
+
+# def attempts(n):
+#     x = 1
+#     while x <= n:
+#         print("Attempt " + str(x))
+#         x += 1
+#     print("Done")
+    
+# attempts(5)
+
+
+# username = get_username()
+# while not valid_username(username):
+# 	print("Invalid username")
+# 	username = get_username()
+
+
+# def count_down(start_number):
+#   while (start_number > 0):
+#     print(start_number)
+#     start_number -= 1
+#   print("Zero!")
+
+# count_down(3)
+
